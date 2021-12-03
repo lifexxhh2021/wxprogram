47c568cf82e6c0db9c0cb2e0f24ae328b9689762
qhuang05 <qhuang05@sina.com>
2021-12-03 08:25:08 +0800

    update

--- pages/main/recruitment.vue
+++ pages/main/recruitment.vue
@@ -1,21 +1,51 @@
 <template>
 	<view class="u-page">
-		<u-swiper :list="banners" height="548"></u-swiper>
-		<view>
-			<text>最新资讯</text>
-			<text>2021永春最新补贴政策</text>
+		<u-swiper :list="banners" height="364"></u-swiper>
+		<view class="u-flex u-padding-left-25 u-padding-right-25 u-margin-top-15 u-margin-bottom-15">
+			<view class="notice-text">		
+				<view>最新</view><view>资讯</view>
+			</view>
+			<view class="flex-1">
+				<u-notice-bar 
+					mode="vertical" 
+					:list="noticeData" 
+					:volume-icon="false" 
+					bg-color="transparent"
+					:font-size="30"
+					padding="20rpx"
+					duration="3000"
+				></u-notice-bar>
+			</view>
 		</view>
-		<view>
-			<u-search 
-				placeholder="请输入搜索内容" 
-				v-model="keyword"
-				height="120"
-				border-color="#ccc"
-				:action-style="{position:'absolute', right: '5rpx'}"
-			></u-search>
+		<view class="u-padding-left-25 u-padding-right-25">
+			<view class="u-flex">
+				<view class="u-flex u-margin-right-15">
+					<span>县城</span>
+					<u-icon name="arrow-down" size="20"></u-icon>
+				</view>
+				<view class="u-relative flex-1">
+					<u-search 
+						placeholder="请输入搜索内容" 
+						v-model="keyword"
+						height="75"
+						border-color="#ccc"
+						:action-style="{position:'absolute', right: '5rpx'}"
+						search-icon=""
+						action-text=" "
+					></u-search>
+					<u-icon name="search" class="search-icon" size="40"></u-icon>
+				</view>
+			</view>
 		</view>
-		<view style="height:400rpx;background:#F0AD4E"></view>
-		<u-sticky :offset-top="0">
+		<view class="more-cat u-text-right u-padding-right-30 u-margin-top-20">更多</view>
+		<view class="catlist clear">
+			<view class="catitem" v-for="item in 8">
+				<u-icon name="icon_delete1" custom-prefix="custom-icon"></u-icon>
+				<view>鞋服纺织</view>
+			</view>
+		</view>
+		<view class="stastics u-padding-25">入驻企业:1554 职位:1.52万 简历:5321 访问:3.64万</view>
+		<u-sticky>
 			<view class="tab-bar" id="tabBar">
 				<u-tabs
 					:list="tabs"
@@ -51,6 +81,7 @@
 				],
 				keyword: '',
 				stickyTop: 0,
+				noticeData: ['2021年永春最新补贴政策', '2021年永春最新补贴政策222'],
 				tabs: [{name: '为您推荐'},{name: '附近职位'},{name: '高薪职位'},{name: '最新发布'}],
 				currentTab: 0,
 				listData: [],
@@ -107,10 +138,36 @@
 </script>
 
 <style lang="scss" scoped>
+	.flex-1{
+		flex: 1;
+	}
+	.search-icon{
+		position: absolute;
+		right: 20rpx;
+		top: 18rpx;
+	}
+	.notice-text{
+		color: #4b97e9;
+		font-weight: bold;
+		font-size: 18px;
+	}
+	.catlist{
+		display: flex;
+		flex-wrap: wrap;
+		.catitem{
+			width: 25%;
+			text-align: center;
+			margin: 15rpx 0;
+		}
+	}
+	.stastics{
+		border-bottom: 15rpx solid #f8f6f9;
+	}
 	.list-wrap {
+		min-height: calc(100vh - 100rpx);
 		padding: 0 20rpx;
-		min-height: calc(100vh - 44px - constant(safe-area-inset-top));
-		min-height: calc(100vh - 44px - env(safe-area-inset-top));
+		// min-height: calc(100vh - 44px - constant(safe-area-inset-top));
+		// min-height: calc(100vh - 44px - env(safe-area-inset-top));
 	}
 	.list-item{
 		width: 345rpx;
