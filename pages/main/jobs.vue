2e1f0cb3677215639cc9101f4410ba38cf08544f
qhuang05 <qhuang05@sina.com>
2021-12-04 08:16:45 +0800

    所有職位

--- pages/main/jobs.vue
+++ pages/main/jobs.vue
@@ -1,6 +1,35 @@
 <template>
 	<view>
-		所有职位
+		<view class="u-m-20">
+			<u-search
+				placeholder="请输入关键字"
+				v-model="keyword"
+				height="75"
+				border-color="#ccc"
+				bg-color="#fff"
+				:action-style="{ position: 'absolute', right: '5rpx' }"
+				action-text=" "
+			></u-search>
+		</view>
+		<view class="categorys u-flex">
+			<view class="left">
+				<view 
+					class="catitem u-flex u-row-between u-col-center u-border-bottom"
+					:class="{'active': index==0}"
+					v-for="item,index in 50"
+				>
+					<view>文职服务类{{index+1}}</view>
+					<u-icon name="play-right-fill" class="icon"></u-icon>
+				</view>
+			</view>
+			<view class="right">
+				<view class="catitem u-flex u-row-between"
+					v-for="item,index in 50"
+				>
+					<view>文秘</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,7 +37,7 @@
 	export default {
 		data() {
 			return {
-				
+				keyword:''
 			}
 		},
 		methods: {
@@ -17,6 +46,33 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+page{
+	background: #f9f9f9;
+}
+.categorys{
+	.left, .right{
+		width: 50%;
+		height: calc(100vh - 115rpx);
+		overflow-y: auto;
+	}
+	.left{
+		background: #fff;
+	}
+	.catitem{
+		height: 100rpx;
+		padding: 0 30rpx;
+		&.active{
+			color: $cs-primary-color;
+			.icon{
+				color: $cs-primary-color;
+			}
+		}
+	}
+	.icon{
+		color: #cacaca;
+	}
+}
+
 
 </style>
--- pages/main/recruitment.vue
+++ pages/main/recruitment.vue
@@ -1,198 +1,218 @@
 <template>
-	<view class="u-page">
-		<u-swiper :list="banners" height="364"></u-swiper>
-		<view class="u-flex u-padding-left-25 u-padding-right-25 u-margin-top-15 u-margin-bottom-15">
-			<view class="notice-text">		
-				<view>最新</view><view>资讯</view>
-			</view>
-			<view class="flex-1">
-				<u-notice-bar 
-					mode="vertical" 
-					:list="noticeData" 
-					:volume-icon="false" 
-					bg-color="transparent"
-					:font-size="30"
-					padding="20rpx"
-					duration="3000"
-				></u-notice-bar>
-			</view>
-		</view>
-		<view class="u-padding-left-25 u-padding-right-25">
-			<view class="u-flex">
-				<view class="u-flex u-margin-right-15">
-					<span>县城</span>
-					<u-icon name="arrow-down" size="20"></u-icon>
-				</view>
-				<view class="u-relative flex-1">
-					<u-search 
-						placeholder="请输入搜索内容" 
-						v-model="keyword"
-						height="75"
-						border-color="#ccc"
-						:action-style="{position:'absolute', right: '5rpx'}"
-						search-icon=""
-						action-text=" "
-					></u-search>
-					<u-icon name="search" class="search-icon" size="40"></u-icon>
-				</view>
-			</view>
-		</view>
-		<view class="more-cat u-text-right u-padding-right-30 u-margin-top-20">更多</view>
-		<view class="catlist clear">
-			<view class="catitem" v-for="item in 8">
-				<u-icon name="icon_delete1" custom-prefix="custom-icon"></u-icon>
-				<view>鞋服纺织</view>
-			</view>
-		</view>
-		<view class="stastics u-padding-25">入驻企业:1554 职位:1.52万 简历:5321 访问:3.64万</view>
-		<u-sticky>
-			<view class="tab-bar" id="tabBar">
-				<u-tabs
-					:list="tabs"
-					:current="currentTab"
-					@change="changeTab"
-					:is-scroll="true" 
-					gutter="30"
-					height="100"
-					bar-width="100"
-					font-size="30"
-					active-color="#FF4D4F"
-					inactive-color="#666"
-				></u-tabs>
-			</view>
-		</u-sticky>
-		<view class="list-wrap u-flex u-flex-wrap">
-			<view class="list-item" v-for="item,index in listData" :key="index">
-				<view>
-					<view class="name">服务员</view>
-					<view class="addr">永春县城关XX街道</view>
-					<view class="education">初中及以下</view>
-				</view>
-				<view class="limit">1-2年</view>
-				<view>
-					<view class="money">2500元-3500元/月</view>
-					<view class="date">10月06日</view>
-				</view>
-			</view>
-		</view>
-		<view class="loading">加载中...</view>
-	</view>
+  <view class="u-page">
+    <u-swiper :list="banners" height="364"></u-swiper>
+    <view class="u-flex u-p-l-25 u-p-r-25 u-m-t-15 u-m-b-15">
+      <view class="notice-text"> <view>最新</view><view>资讯</view> </view>
+      <view class="u-flex-1">
+        <u-notice-bar
+          mode="vertical"
+          :list="noticeData"
+          :volume-icon="false"
+          bg-color="transparent"
+          :font-size="30"
+          padding="20rpx"
+          duration="3000"
+        ></u-notice-bar>
+      </view>
+    </view>
+    <view class="u-p-l-25 u-p-r-25">
+      <view class="u-flex">
+        <view class="u-flex u-m-r-15">
+          <span>县城</span>
+          <u-icon name="arrow-down" size="20"></u-icon>
+        </view>
+        <view class="u-relative u-flex-1">
+          <u-search
+            placeholder="请输入搜索内容"
+            v-model="keyword"
+            height="75"
+            border-color="#ccc"
+            bg-color="#fff"
+            :action-style="{ position: 'absolute', right: '5rpx' }"
+            search-icon=""
+            action-text=" "
+          ></u-search>
+          <u-icon name="search" class="search-icon" size="40"></u-icon>
+        </view>
+      </view>
+    </view>
+    <view class="more-cat u-text-right u-m-t-20 u-m-r-20">
+      更多
+      <u-icon name="arrow-right-double"></u-icon>
+    </view>
+    <view class="catlist">
+      <view class="catitem" v-for="item in 8">
+        <u-icon
+          name="icon_delete1"
+          custom-prefix="custom-icon"
+          size="80"
+          color="#6867cf"
+        ></u-icon>
+        <view class="text">鞋服纺织</view>
+      </view>
+    </view>
+    <view class="stastics u-p-25">入驻企业:1554 职位:1.52万 简历:5321 访问:3.64万</view>
+    <u-sticky>
+      <view class="tab-bar" id="tabBar">
+        <u-tabs
+          :list="tabs"
+          :current="currentTab"
+          @change="changeTab"
+          :is-scroll="true"
+          item-width="250"
+          height="96"
+          bar-width="74"
+          font-size="32"
+          active-color="#0090f4"
+          bar-style="{'text-align':'center'}"
+        ></u-tabs>
+      </view>
+    </u-sticky>
+    <view class="list-wrap">
+      <view class="list-item" v-for="(item, index) in listData" :key="index">
+        <view>
+          <view class="name">服务员</view>
+          <view class="addr u-m-t-10 u-line-1">永春县城关XX街道</view>
+          <view class="education">初中及以下</view>
+        </view>
+        <view class="limit u-m-t-32">1-2年</view>
+        <view class="u-flex-col u-text-right">
+          <view class="money">2500元-3500元/月</view>
+          <view class="date u-m-t-30">10月06日</view>
+        </view>
+      </view>
+      <view class="loading" v-if="isLoading">加载中...</view>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				banners: [
-					{ image: 'https://cdn.uviewui.com/uview/swiper/1.jpg' },
-					{ image: 'https://cdn.uviewui.com/uview/swiper/2.jpg' },
-					{ image: 'https://cdn.uviewui.com/uview/swiper/3.jpg' },
-				],
-				keyword: '',
-				stickyTop: 0,
-				noticeData: ['2021年永春最新补贴政策', '2021年永春最新补贴政策222'],
-				tabs: [{name: '为您推荐'},{name: '附近职位'},{name: '高薪职位'},{name: '最新发布'}],
-				currentTab: 0,
-				listData: [],
-				isLoading: false,
-			}
-		},
-		methods: {
-			async getData(isReload){
-				if(isReload) this.listData = [];
-				if(this.isLoading) return;
-				this.isLoading = true;
-				// let ret = this.$u.api.getList();
-				setTimeout(()=>{
-					this.isLoading = false;
-					for(let i=0; i<20; i++){
-						this.listData.push(i);
-					}
-				}, 1000);
-			},
-			changeTab(index){
-				this.currentTab = index;
-				uni.createSelectorQuery().selectViewport().scrollOffset(res => {
-					if(res.scrollTop > this.tabBarTop){	
-						uni.pageScrollTo({
-							scrollTop: this.tabBarTop,
-							duration: 0
-						});
-					}
-				}).exec();
-				this.getData(true);
-			},
-			onLoad(options){
-				console.log('页面参数：', options);
-			},
-			onShow(){
-				this.getData(true);
-				
-				let sysInfo = uni.getSystemInfoSync();
-				let _top = sysInfo.statusBarHeight + 44;
-				// #ifndef H5
-					// px转rpx
-					this.stickyTop = _top/(uni.upx2px(_top)/_top);
-				// #endif
-				uni.createSelectorQuery().select('#tabBar').boundingClientRect(res => {
-					this.tabBarTop = res.top - _top;
-				}).exec();
-			},
-			onReachBottom(){
-				this.getData();
-			}
-		},
-		
-	}
+export default {
+  data() {
+    return {
+      banners: [
+        { image: "https://cdn.uviewui.com/uview/swiper/1.jpg" },
+        { image: "https://cdn.uviewui.com/uview/swiper/2.jpg" },
+        { image: "https://cdn.uviewui.com/uview/swiper/3.jpg" },
+      ],
+      keyword: "",
+      stickyTop: 0,
+      noticeData: ["2021年永春最新补贴政策", "2021年永春最新补贴政策222"],
+      tabs: [{ name: "为您推荐" }, { name: "热门职位" }, { name: "最新发布" }],
+      currentTab: 0,
+      listData: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async getData(isReload) {
+      if (isReload) this.listData = [];
+      if (this.isLoading) return;
+      this.isLoading = true;
+      // let ret = this.$u.api.getList();
+      setTimeout(() => {
+        this.isLoading = false;
+        for (let i = 0; i < 10; i++) {
+          this.listData.push(i);
+        }
+      }, 1000);
+    },
+    changeTab(index) {
+      this.currentTab = index;
+      uni.createSelectorQuery().selectViewport().scrollOffset((res) => {
+				if (res.scrollTop > this.tabBarTop) {
+					uni.pageScrollTo({
+						scrollTop: this.tabBarTop,
+						duration: 0,
+					});
+				}
+			}).exec();
+      this.getData(true);
+    },
+  },
+  onLoad(options) {
+    console.log("页面参数：", options);
+  },
+  onShow() {
+    this.getData(true);
+
+    // let sysInfo = uni.getSystemInfoSync();
+    // let _top = sysInfo.statusBarHeight + 44;
+    // #ifndef H5
+    // px转rpx
+    // this.stickyTop = _top / (uni.upx2px(_top) / _top);
+    // #endif
+    uni
+      .createSelectorQuery()
+      .select("#tabBar")
+      .boundingClientRect((res) => {
+        // this.tabBarTop = res.top - _top;
+				this.tabBarTop = res.top;
+      })
+      .exec();
+  },
+  onReachBottom() {
+    this.getData();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-	.flex-1{
-		flex: 1;
-	}
-	.search-icon{
-		position: absolute;
-		right: 20rpx;
-		top: 18rpx;
-	}
-	.notice-text{
-		color: #4b97e9;
-		font-weight: bold;
-		font-size: 18px;
-	}
-	.catlist{
-		display: flex;
-		flex-wrap: wrap;
-		padding: 0 20px;
-		.catitem{
-			width: 25%;
-			text-align: center;
-			margin:20rpx 0;
-		}
-	}
-	.stastics{
-		border-bottom: 15rpx solid #f8f6f9;
-	}
-	.list-wrap {
-		min-height: calc(100vh - 100rpx);
-		padding: 0 20rpx;
-		// min-height: calc(100vh - 44px - constant(safe-area-inset-top));
-		// min-height: calc(100vh - 44px - env(safe-area-inset-top));
-	}
-	.list-item{
-		width: 100%;
-		display:flex;
-		justify-content: space-between;
-		align-items: center;
-		margin-top:10px;
-		.name{
-		}
-		.money{
-			color:#f0685c;
-		}
-	}
-	.loading{
-		text-align: center;
-		padding: 30rpx 0;
-	}
+.search-icon {
+  position: absolute;
+  right: 20rpx;
+  top: 18rpx;
+}
+.notice-text {
+  color: $cs-primary-color;
+  font-weight: bold;
+  font-size: 32rpx;
+}
+.catlist {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 20rpx;
+  .catitem {
+    width: 25%;
+    text-align: center;
+    margin: 20rpx 0;
+    .text {
+      color: #847d7d;
+      margin-top: 15rpx;
+      font-size: 30rpx;
+    }
+  }
+}
+.stastics {
+  border-bottom: 15rpx solid #f8f6f9;
+}
+.list-wrap {
+  // min-height: calc(100vh - 100rpx);
+  min-height: 2000rpx;
+  padding: 0 20rpx;
+  // min-height: calc(100vh - 44px - constant(safe-area-inset-top));
+  // min-height: calc(100vh - 44px - env(safe-area-inset-top));
+}
+.list-item {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 15px;
+  .name {
+    font-size: 32rpx;
+    color: #2a272c;
+    font-weight: bold;
+  }
+  .limit {
+    align-self: center;
+  }
+  .money {
+    color: #f0685c;
+    font-size: 32rpx;
+  }
+}
+.loading {
+  text-align: center;
+  padding: 30rpx 0;
+}
 </style>
