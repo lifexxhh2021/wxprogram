8b23a8f52dea82235cc67e4dc04d31d2415114dd
qhuang05 <qhuang05@sina.com>
2021-12-05 16:40:10 +0800

    upload

--- pages/jobs/register2.vue
+++ pages/jobs/register2.vue
@@ -2,7 +2,7 @@
 	<view>
 		<view class="content u-p-l-30 u-p-r-30">
 			<u-form :model="form" ref="uForm" :error-type="['message']">
-				<u-form-item label="头像" prop="photo">
+				<u-form-item label="公司Logo" prop="photo" label-width="160">
 					<u-upload 
 						ref="uUpload"
 						:action="uploadAction" 
@@ -20,57 +20,56 @@
 						</view>
 					</u-upload>
 				</u-form-item>
-				<u-form-item label="姓名" prop="name">
-					<u-input v-model="form.name" input-align="right" placeholder="请输入姓名" />
+				<u-form-item label="公司全称" prop="name" label-width="160">
+					<u-input v-model="form.company" input-align="right" placeholder="请输入公司全称" />
 				</u-form-item>
-				<u-form-item label="性别" prop="gender">
-					<view class="u-flex u-row-right">						
-						<u-radio-group v-model="form.gender">
-							<u-radio shape="circle" name="1">男</u-radio>
-							<u-radio shape="circle" name="2">女</u-radio>
-						</u-radio-group>
-					</view>
+				<u-form-item label="联系人" prop="name" label-width="160">
+					<u-input v-model="form.name" input-align="right" placeholder="请输入联系人" />
 				</u-form-item>
-				<u-form-item label="出生年月" prop="date" label-width="160">
+				<u-form-item label="联系电话" prop="mobile" label-width="160">
+					<u-input v-model="form.mobile" input-align="right" maxlength="15" placeholder="请输入联系电话" />
+				</u-form-item>
+				<u-form-item label="成立时间" prop="date" label-width="160">	
 					<view class="u-flex u-row-right">
 						<u-input v-model="dateStr" input-align="right" placeholder="请选择" disabled @click="showPicker('date')" />
 						<u-icon name="arrow-right" class="u-m-l-15 gray"></u-icon>
 					</view>
 				</u-form-item>
-				<u-form-item label="最高学历" prop="education" label-width="160">
-					<view class="u-flex u-row-right">						
+				<u-form-item label="所属行业" prop="education" label-width="160">
+					<view class="u-flex u-row-right">			
 						<u-input v-model="form.education" input-align="right" placeholder="请选择" disabled @click="showPicker('education')" />
 						<u-icon name="arrow-right" class="u-m-l-15 gray"></u-icon>
 					</view>
 				</u-form-item>
-				<u-form-item label="工作时间" prop="experience" label-width="160">
+				<u-form-item label="公司规模" prop="experience" label-width="160">
 					<view class="u-flex u-row-right">						
 						<u-input v-model="form.experience" input-align="right" placeholder="请选择" disabled @click="showPicker('experience')" />
 						<u-icon name="arrow-right" class="u-m-l-15 gray"></u-icon>
 					</view>
 				</u-form-item>
-				<u-form-item label="意向岗位" prop="profession" label-width="160">
+				<view class="split-line"></view>
+				<u-form-item label="公司地址" prop="province" label-width="160">
 					<view class="u-flex u-row-right">
-						<u-input v-model="form.profession" input-align="right" placeholder="请选择" disabled @click="showPicker('profession')" />
-						<u-icon name="arrow-right" class="u-m-l-15 gray"></u-icon>
+						<u-input v-model="addressStr" input-align="right" placeholder="请选择" disabled @click="showPicker('address')" />
+					</view>
+				</u-form-item>
+				<u-form-item label="详细地址" prop="street" label-width="160">
+					<view class="u-flex u-row-right">
+						<u-input v-model="form.street" input-align="right" placeholder="请填写街道、楼牌号等" />
 					</view>
 				</u-form-item>
 				<view class="split-line"></view>
-				<u-form-item label="手机" prop="mobile">
-					<u-input v-model="form.mobile" input-align="right" placeholder="请输入手机号" />
-				</u-form-item>
-				<u-form-item label="邮箱(选填)" label-width="160">
-					<u-input v-model="form.email" input-align="right" placeholder="请输入邮箱" />
-				</u-form-item>
+				<view class="u-p-t-30 u-p-b-30">
+					<view>营业执照/其他有效证明<text class="gray">（最多上传3张）</text></view>
+					<u-upload :action="action" :file-list="fileList" ></u-upload>
+					<view class="tips gray">提示：此项只用于平台审核，不会对外展示</view>
+				</view>
 				<view class="split-line"></view>
-				<u-form-item label="简历状态" prop="status" label-width="160">
-					<view class="u-flex u-row-right">						
-						<u-radio-group v-model="form.status">
-							<u-radio shape="circle" name="1">公开</u-radio>
-							<u-radio shape="circle" name="2">不公开</u-radio>
-						</u-radio-group>
-					</view>
-				</u-form-item>
+				<view class="u-p-t-30 u-p-b-30">
+					<view>公司简介</view>
+					<u-input v-model="form.desc" type="textarea" :height="120" :auto-height="true" maxlength="1500" />
+				</view>
+				<view class="split-line"></view>
 			</u-form>
 			<view class="u-flex u-col-center u-m-t-30 u-m-b-30">
 				<u-checkbox v-model="form.agree">我已阅读同意</u-checkbox>
@@ -81,12 +80,12 @@
 		<view class="fixed-bottom" @click="save">保 存</view>
 		<!-- 日期选择 -->
 		<u-picker v-model="isShowDatePicker" mode="time" @confirm="dateConfirm"></u-picker>
+		<!-- 地址选择 -->
+		<u-picker v-model="isShowAddrPicker" mode="region" @confirm="addressConfirm"></u-picker>
 		<!-- 学历选择 -->
 		<u-select v-model="isShowEducationPicker" mode="single-column" :list="educationData" @confirm="educationConfirm"></u-select>
 		<!-- 工作时间选择 -->
 		<u-select v-model="isShowExperiencePicker" mode="single-column" :list="experienceData" @confirm="experienceConfirm"></u-select>
-		<!-- 意向岗位选择 -->
-		<u-select v-model="isShowProfessionPicker" mode="single-column" :list="professionData" @confirm="professionConfirm"></u-select>
 	</view>
 	
 </template>
@@ -98,8 +97,11 @@
 				form: {
 					photo: 'https://cdn.uviewui.com/uview/swiper/1.jpg',
 					name: '',
-					gender: '1',
 					date: '',
+					province: '',
+					city: '',
+					area: '',
+					street: '',
 					education: '',
 					experience: '',
 					profession: '',
@@ -162,6 +164,8 @@
 				},
 				uploadAction: 'http://www.example.com/upload',
 				photoList: [],
+				action: 'http://www.example.com/upload',
+				fileList: [],
 				isShowDatePicker: false,
 				dateParams: {
 					year: true,
@@ -171,6 +175,7 @@
 					minute: false,
 					second: false
 				},
+				isShowAddrPicker: false,
 				isShowEducationPicker: false,
 				educationData: [
 					{value: 1, label: '应届毕业'}, 
@@ -182,16 +187,14 @@
 					{value: 1, label: '1-3年'}, 
 					{value: 2, label: '3-5年'}
 				],
-				isShowProfessionPicker: false,
-				professionData: [
-					{value: 1, label: '技术岗'},
-					{value: 2, label: '销售岗'}
-				]
 			}
 		},
 		computed: {
-			dateStr() {
+			dateStr(){
 				return this.form.date ? this.form.date.join('-') : ''
+			},
+			addressStr() {
+				return this.form.province ? this.form.province + ' ' + this.form.city + ' ' + this.form.area : ''
 			}
 		},
 		methods: {
@@ -205,14 +208,14 @@
 					case 'date':
 						this.isShowDatePicker = true;
 						break;
+					case 'address':
+						this.isShowAddrPicker = true;
+						break;
 					case 'education':
 						this.isShowEducationPicker = true;
 						break;
 					case 'experience':
 						this.isShowExperiencePicker = true;
-						break;
-					case 'profession':
-						this.isShowProfessionPicker = true;
 						break;
 					default:
 						break;
@@ -222,14 +225,17 @@
 				console.log('dateConfirm>>', e);
 				this.form.date = [e.year, e.month, e.day];
 			},
+			addressConfirm(e){
+				console.log('addressConfirm>>',e);
+				this.form.province = e.province.label;
+				this.form.city = e.city.label;
+				this.form.area = e.area.label;
+			},
 			educationConfirm(e){
 				this.form.education = e[0].label;
 			},
 			experienceConfirm(e){
 				this.form.experience = e[0].label;
-			},
-			professionConfirm(e){
-				this.form.profession = e[0].label;
 			},
 			save() {
 				console.log('form>>', this.form)
